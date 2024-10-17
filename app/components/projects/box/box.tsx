d4f57947483939a1b5d41c2eef1c6fc3a4e3612c
frontcodelover <contact@avantjetaisriche.com>
2024-10-17 21:38:15 +0200

    FEAT: projects added

--- app/components/projects/box/box.tsx
+++ app/components/projects/box/box.tsx
@@ -6,15 +6,25 @@ interface BoxProps {
   text: string;
   colors: string;
   headings: string;
+  stack: Array<string>;
   link: string;
 }
 
-export const Box: React.FC<BoxProps> = ({ style, text, colors, headings, link }) => {
+export const Box: React.FC<BoxProps> = ({ style, text, colors, headings, stack, link }) => {
   return (
-    <div className={`${style} ${colors} rounded-3xl text-slate-200`}>
-      <h3 className='font-bold md:text-2xl text-xl'>{headings}</h3>
-      <div className='lg:text-5xl text-4xl'>{text}</div>
-      <Link href={link}></Link>
+    <div className={`${style} ${colors} rounded-3xl text-slate-200 h-full`}>
+      <div className='flex flex-col justify-between h-full'>
+        <div className='flex flex-col gap-6'>
+          <h3 className='font-bold md:text-2xl text-xl'>{headings}</h3>
+          <div className='md:text-4xl text-3xl text-balance'>{text}</div>
+          <Link href={link}></Link>
+        </div>
+				<div className='text-sm flex items-end gap-2 justify-end w-full flex-wrap'>{
+					stack.map((item, index) => (
+						<span className='bg-emerald-500 text-slate-50 px-2 rounded-xl tracking-normal' key={index}>{item} </span>
+					))
+				}</div>
+      </div>
     </div>
   );
 };
--- app/components/projects/projects.tsx
+++ app/components/projects/projects.tsx
@@ -1,42 +1,75 @@
 'use client';
 import React from 'react';
 import { Box } from './box/box';
-import { useGitHubCommits } from '@/app/hooks/useGitHubCommits'; // Utilise le hook
+import { useGitHubCommits } from '@/app/hooks/useGitHubCommits';
+import { motion } from 'framer-motion';
 
 export const Projects = () => {
-  const { commits, loading, error,totalCount } = useGitHubCommits();
+  const { commits, loading, error, totalCount } = useGitHubCommits();
 
-	console.log(commits, loading, error, totalCount);
+  console.log(commits, loading, error, totalCount);
 
   return (
     <>
-      <div id='projects' />
-      <div className='flex gap-7'>
-        <Box
-          style={'min-h-[350px] lg:min-h-[500px] flex flex-col gap-7 p-[30px] lg:p-[50px] w-full md:w-[calc(33.33%-10px)]'}
-          colors={'bg-slate-300 text-slate-900'}
-          headings={'Learning'}
-          text={'A front developper is always learning new things.'}
-          link={'/projects'}
-        />
-        <Box
-          style={'min-h-[350px] lg:min-h-[500px] flex flex-col gap-7 p-[30px] lg:p-[50px] w-full md:w-[calc(33.33%-10px)]'}
-          colors={'bg-slate-300 text-slate-900'}
-          headings={'Learning'}
-          text={'A front developper is always learning new things.'}
-          link={'/projects'}
-        />
-        <Box
-          style={'min-h-[350px] lg:min-h-[500px] flex flex-col gap-7 p-[30px] lg:p-[50px] w-full md:w-[calc(33.33%-10px)]'}
-          colors={'bg-slate-300 text-slate-900'}
-          headings={'Learning'}
-          text={'A front developper is always learning new things.'}
-          link={'/projects'}
-        />
-			</div>
-			<div className='flex gap-7 text-7xl'>
-        All this after {totalCount}
-			</div>
+      <div>
+        <div id="projects" />
+        <div className="grid lg:grid-cols-3 gap-7 w-full">
+          {/* Première Box avec whileInView */}
+          <motion.div
+            initial={{ opacity: 0, scale: 0.9 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 0.3, duration: 0.8, ease: 'easeOut' }}
+            viewport={{ once: true, amount: 0.2 }} 
+          >
+            <Box
+              style={'min-h-[350px] lg:min-h-[500px] flex flex-col gap-7 p-[30px] lg:p-[50px] w-full'}
+              colors={'bg-slate-300 text-slate-900'}
+              headings={'Photographer MarketPlace'}
+              text={'A market place for photographers who want to sell their photos.'}
+              stack={['NextJS', 'Supabase', 'TypeScript', 'TailwindCSS']}
+              link={'/projects'}
+            />
+          </motion.div>
+
+          {/* Deuxième Box avec whileInView et un délai supplémentaire */}
+          <motion.div
+            initial={{ opacity: 0, scale: 0.9 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 0.6, duration: 0.8, ease: 'easeOut' }}
+            viewport={{ once: true, amount: 0.2 }} 
+          >
+            <Box
+              style={'min-h-[350px] lg:min-h-[500px] flex flex-col gap-7 p-[30px] lg:p-[50px] w-full'}
+              colors={'bg-slate-300 text-slate-900'}
+              headings={'BetweenDevs'}
+              text={'An app to find developers who use the same technologies as you.'}
+              stack={['React', 'TailwindCSS', 'NodeJS', 'WebSocket']}
+              link={'/projects'}
+            />
+          </motion.div>
+
+          {/* Troisième Box avec whileInView et un délai encore plus grand */}
+          <motion.div
+            initial={{ opacity: 0, scale: 0.9 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ delay: 0.9, duration: 0.8, ease: 'easeOut' }}
+            viewport={{ once: true, amount: 0.2 }}  
+          >
+            <Box
+              style={'min-h-[350px] lg:min-h-[500px] flex flex-col gap-7 p-[30px] lg:p-[50px] w-full'}
+              colors={'bg-slate-300 text-slate-900'}
+              headings={'Favorite books'}
+              text={'An app to save your favorite books.'}
+              stack={['React', 'Firebase', 'TailwindCSS']}
+              link={'/projects'}
+            />
+          </motion.div>
+        </div>
+
+        <div className="text-3xl ml-[66%] w-3/6 py-20 tracking-tighter">
+          Just few projects after <span className="font-black">{totalCount} commits</span>
+        </div>
+      </div>
     </>
   );
 };
