86fd6e42d172c014778daaae3a6dfb532a351bea
frontcodelover <contact@avantjetaisriche.com>
2024-10-31 11:35:38 +0100

    STYLE: change form & heading commas

--- app/components/contact/form/contactForm.tsx
+++ app/components/contact/form/contactForm.tsx
@@ -6,29 +6,42 @@ const ContactForm = () => {
   const [state, handleSubmit] = useForm('mlevbnbz');
 
   if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
+    return <p className='text-xl text-center'>Merci pour votre message, je vous répondrai dans les plus brefs délais.</p>;
   }
 
   return (
     <div className='mt-20'>
       <form onSubmit={handleSubmit}>
-        <div className='flex flex-col gap-6 '>
-          <label htmlFor='email' className='dark:text-white text-3xl tracking-tighter'>
+        <div className='flex flex-col gap-6 text-slate-800 tracking-tight'>
+          
+					<label htmlFor='name' className='md:text-3xl text-xl'>
+						Votre nom
+					</label>
+					<input id='name' type='text' name='name' className='rounded-2xl p-3  border border-slate-500' />
+					<ValidationError prefix='Name' field='name' errors={state.errors} />
+
+					<label htmlFor='surname' className='md:text-3xl text-xl'>
+						Votre prénom
+					</label>
+					<input id='surname' type='text' name='surname' className='rounded-2xl p-3  border border-slate-500' />
+					<ValidationError prefix='Surname' field='surname' errors={state.errors} />
+					
+					<label htmlFor='email' className='md:text-3xl text-xl'>
             Votre email
           </label>
-          <input id='email' type='email' name='email' className='rounded-2xl p-3 text-slate-900 dark:border-none border' />
+          <input id='email' type='email' name='email' className='rounded-2xl p-3  border border-slate-500' />
           <ValidationError prefix='Email' field='email' errors={state.errors} />
 
-          <label htmlFor='message' className='dark:text-white text-3xl tracking-tighter'>
+          <label htmlFor='message' className=' md:text-3xl text-xl'>
             Votre message
           </label>
-          <textarea id='message' name='message' className=' text-slate-900 rounded-2xl p-3 h-40  dark:border-none border' />
+          <textarea id='message' name='message' className='  border border-slate-500 rounded-2xl p-3 h-40' />
           <ValidationError prefix='Message' field='message' errors={state.errors} />
 
           <button
             type='submit'
             disabled={state.submitting}
-            className='bg-white px-4 py-1 rounded-2xl text-3xl text-slate-900 hover:text-slate-800 font-black tracking-tighter  dark:border-none border'
+            className='px-4 py-1 rounded-2xl md:text-3xl text-xl border text-slate-100 bg-emerald-600 hover:bg-emerald-700 transition ease-in-out hover:text-slate-200 font-black tracking-tighter '
           >
             Envoyer
           </button>
